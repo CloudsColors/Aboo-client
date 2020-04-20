e75a5f2981b39a495ec77e9253cb1b1150a85b32
cloudscolors <andreas.mansson@live.com>
2020-04-20 18:14:18 +0200

    Added draw rect functionality with mouse and escape to close window

--- windows.py
+++ windows.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QPainter, QBrush, QPen, QCursor
 from PyQt5 import QtCore
 from keylistener import KeyListener
 
@@ -34,14 +35,42 @@ class CanvasWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super(CanvasWindow, self).__init__()
+        self.mousePressed = None
+        self.mouseNow = None
         self.top = 0
         self.left = 0
         self.width = 1920
         self.height = 1080
         self.init_window()
+
+    def keyPressEvent(self, e):
+        if(e.key() == QtCore.Qt.Key_Escape):
+            self.close()
+
+    def mouseMoveEvent(self, e):
+        self.mouseNow = e.pos()
+        self.update()
+
+    def mousePressEvent(self, e):
+        self.mousePressed = e.pos()
+
+    def mouseReleaseEvent(self, e):
+        #todo: Make opacity 0.0
+        #todo: take screenshot over area
+        #todo: close the window
+        return
     
     def init_window(self):
         self.setGeometry(self.top, self.left, self.width, self.height)
-        self.setWindowOpacity(0.7)
+        self.setWindowOpacity(0.9)
         #Remove titlebar
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+    def paintEvent(self, event):
+        if(self.mouseNow == None or self.mousePressed == None):
+            return
+        self.painter = QPainter(self)
+        self.painter.setPen(QPen(QtCore.Qt.black, 1, QtCore.Qt.SolidLine))
+        x1, y1, x2, y2 = self.mousePressed.x(), self.mousePressed.y(), self.mouseNow.x(), self.mouseNow.y()
+        self.painter.drawRect(x1, y1, x2-x1, y2-y1)
+        self.painter.end()
