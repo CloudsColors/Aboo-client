dec7badfa3f25194f4facb33be5327f16ade31bc
cloudscolors <andreas.mansson@live.com>
2020-04-21 22:16:51 +0200

    started making label for when a new upload is done, needs more work, incomplete.

--- window.py
+++ window.py
@@ -30,7 +30,11 @@ class Window(QMainWindow):
         self.dialogs.clear()
 
     def on_hotkey_create_canvas(self):
+        #Clear any windows that already are open (to prevent several layers of windows)
+        self.on_close_canvases()
+        #Read in nr of monitors the user has
         nrOfScreens = QDesktopWidget().screenCount()
+        #Loop through and start the canvas on each of the monitor.
         for i in range(0, nrOfScreens):
             screenGeometry = QDesktopWidget().availableGeometry(i)
             self.dialog = CanvasWindow(screenGeometry.x(), screenGeometry.y(), self)
@@ -42,21 +46,29 @@ class Window(QMainWindow):
     def init_window(self):
         #Logo
         self.logo = QLabel(self)
-        pixmap = QPixmap('art/logo.png')
+        pixmap = QPixmap('statics/logo.png')
         self.logo.setPixmap(pixmap)
         self.logo.setGeometry(0,0,800,200)
         self.logo.setStyleSheet("background-color: #3D60A7")
-
         #Canvas windows list
         self.dialogs = list()
         #Window settings
         self.setStyleSheet("background-color: #A0D2E7;")
-        self.setWindowIcon(QIcon("art/icon-medium.png"))
+        self.setWindowIcon(QIcon("statics/icon-medium.png"))
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
         self.setMaximumSize(self.width, self.height)
         #Show the window
+        self.create_new_upload_bubble()
         self.show()
+
+    def create_new_upload_bubble(self):
+        self.uploadText = QLabel(self)
+        self.uploadText.setText("<h3> New entry </h3> \n <a href='https://www.aboo.se/'> Link </a> \n TTL: 2020-04-22 21:11")
+        self.uploadText.setGeometry(5, 220, 390, 100)
+        self.uploadText.setStyleSheet("background-color: #81B1D5; border: 1px solid black; border-radius: 5px")
+
+
 
     #todo: import webbrowser
     #todo: webbrowser.open(url)
